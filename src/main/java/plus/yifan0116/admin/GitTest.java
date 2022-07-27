cd7ea4f18657be1baac05e4bcf8daa4b4fd4b0c8
Wyf <691506722@qq.com>
2022-07-27 18:26:08 +0800

    third commit

--- src/main/java/plus/yifan0116/admin/GitTest.java
+++ src/main/java/plus/yifan0116/admin/GitTest.java
@@ -3,6 +3,7 @@ package plus.yifan0116.admin;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("hello git!");
+        System.out.println("hello git!2");
+        System.out.println("hello git!3");
     }
 }

263b6f855ed9613e7aabb5e3ae04cf16ad329592
Wyf <691506722@qq.com>
2022-07-27 18:25:33 +0800

    second commit

--- src/main/java/plus/yifan0116/admin/GitTest.java
+++ src/main/java/plus/yifan0116/admin/GitTest.java
@@ -3,5 +3,6 @@ package plus.yifan0116.admin;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 }
